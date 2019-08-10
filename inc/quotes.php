3934251f0a72308db86ab061f7d7b15c4c822d5c
Cody Perry <perrysdevelopment@gmail.com>
2019-08-10 12:37:15 -0400

    Add random quote generator files

---
+++ inc/quotes.php
@@ -0,0 +1,84 @@
+<?php
+
+$quotes = [
+  [
+    'quote' => 'The Force will be with you. Always',
+    'source' => 'Obi-Wan Kenobi',
+    'category' => 'Star Wars'
+  ],
+  [
+    'quote' => 'Your weapon… you will not need it',
+    'source' => 'Yoda',
+    'category' => 'Star Wars'
+  ],
+  [
+    'quote' => 'Live life to the fullest, and focus on the positive',
+    'source' => 'Matt Cameron',
+    'category' => 'Focus Quotes'
+  ],
+  [
+    'quote' => 'Over the years, I’ve learned, focus on the job at hand, and opportunities will open after',
+    'source' => 'Antonio Villaraigosa',
+    'category' => 'Focus Quotes'
+  ],
+  [
+    'quote' => 'The two most powerful warriors are patience and time',
+    'source' => 'Leo Tolstoy',
+    'category' => 'Time'
+  ],
+  [
+    'quote' => 'Patience and fortitude conquer all things',
+    'source' => 'Ralph Waldo Emerson',
+    'category' => 'Patience'
+  ],
+  [
+    'quote' => 'Ninety percent of leadership is the ability to communicate something people want',
+    'source' => 'Dianne Feinstein',
+    'category' => 'Leadership'
+  ],
+  [
+    'quote' => 'The growth and development of people is the highest calling of leadership',
+    'source' => 'Harvey S. Firestone',
+    'category' => 'Leadership'
+  ],
+  [
+    'quote' => 'Actions, not words, are the ultimate results of leadership',
+    'source' => 'Bill Owens',
+    'category' => 'Leadership'
+    ],
+  [
+    'quote' => 'Perseverance is not a long race; it is many short races one after another',
+    'source' => 'Walter Elliott',
+    'category' => 'Perseverance'
+  ],
+  [
+    'quote' => 'Control your own destiny or someone else will',
+    'source' => 'Jack Welch',
+    'category' => 'Destiny'
+  ],
+  [
+    'quote' => 'I do believe that one way to have a destiny is to choose one',
+    'source' => 'Melinda McGraw',
+    'category' => 'Destiny'
+  ],
+  [
+    'quote' => 'Self-belief and hard work will always earn you success',
+    'source' => 'Virat Kohli',
+    'category' => 'Success'
+  ],
+  [
+    'quote' => 'Success is the sum of small efforts – repeated day in and day out',
+    'source' => 'Robert Collier',
+    'category' => 'Success'
+  ],
+  [
+    'quote' => 'Success is not a good teacher, failure makes you humble',
+    'source' => 'Shah Rukh Khan',
+    'category' => 'Success'
+  ],
+  [
+    'quote' => 'Life isn’t about waiting for the storm to pass, it’s about learning to dance in the rain',
+    'source' => 'Vivian Greene',
+    'category' => 'Life'
+  ]
+];
